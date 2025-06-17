af7ead96ab0f56e7ecc86aa4c0ae8e75dccb0151
tionis <me@tionis.dev>
2025-06-17 18:01:42 +0200

    don't try to use starttls on port 25

--- moerderspiel/notification/email.py
+++ moerderspiel/notification/email.py
@@ -21,7 +21,8 @@ def send_message(to: str, subject: str, body: str, attachment: bytes = None, att
             lmtp.quit()
     else:
         with SMTP(config.EMAIL_SMTP_HOST, port=int(config.EMAIL_SMTP_PORT)) as smtp:
-            smtp.starttls()
+            if config.EMAIL_SMTP_PORT != "25":
+                smtp.starttls()
             smtp.ehlo(config.EMAIL_HELO_HOSTNAME)
             if config.EMAIL_SMTP_USER and config.EMAIL_SMTP_PASSWORD:
                 smtp.login(config.EMAIL_SMTP_USER, config.EMAIL_SMTP_PASSWORD)
